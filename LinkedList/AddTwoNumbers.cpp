c10b6a6b8bfc3b454f097225dae61bc66a731f5e
Saurabh Kushwaha <saurabh@Saurabhs-MacBook-Air.local>
2023-06-30 17:09:03 +0530

    done.

--- LinkedList/AddTwoNumbers.cpp
+++ LinkedList/AddTwoNumbers.cpp
@@ -19,7 +19,7 @@ using namespace std;
 // Output: [1]
 
 // Definition for singly-linked list.
-struct ListNode {
+struct ListNode{
     int val;
     ListNode* next;
 
@@ -29,17 +29,17 @@ struct ListNode {
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if (head == nullptr || (head->next == nullptr && n == 1)) {
+        if (head == nullptr || (head->next == nullptr && n == 1)){
             return nullptr;
         }
 
         ListNode* curr;
         int count = 0;
-        for (curr = head; curr->next != nullptr; curr = curr->next) {
+        for (curr = head; curr->next != nullptr; curr = curr->next){
             count++;
         }
 
-        if (n - 1 == count) {
+        if (n - 1 == count){
             ListNode* w = head->next;
             w->next = head->next->next;
             delete head;
@@ -50,11 +50,11 @@ public:
         ListNode* first = temp;
         ListNode* second = temp;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++){
             first = first->next;
         }
 
-        while (first->next != nullptr) {
+        while (first->next != nullptr){
             first = first->next;
             second = second->next;
         }
@@ -67,7 +67,7 @@ public:
     }
 };
 
-int main() {
+int main(){
     Solution solution;
 
     // Example 1:
@@ -80,7 +80,7 @@ int main() {
 
     ListNode* result1 = solution.removeNthFromEnd(head1, n1);
     ListNode* curr1 = result1;
-    while (curr1 != nullptr) {
+    while (curr1 != nullptr){
         std::cout << curr1->val << " ";
         curr1 = curr1->next;
     }
@@ -92,7 +92,7 @@ int main() {
 
     ListNode* result2 = solution.removeNthFromEnd(head2, n2);
     ListNode* curr2 = result2;
-    while (curr2 != nullptr) {
+    while (curr2 != nullptr){
         std::cout << curr2->val << " ";
         curr2 = curr2->next;
     }
@@ -105,7 +105,7 @@ int main() {
 
     ListNode* result3 = solution.removeNthFromEnd(head3, n3);
     ListNode* curr3 = result3;
-    while (curr3 != nullptr) {
+    while (curr3 != nullptr){
         std::cout << curr3->val << " ";
         curr3 = curr3->next;
     }
